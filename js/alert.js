3a15ebf13de95229b6bcbae58c0fe4b2a1f48e05
jahangir <jahangir.ali@easyjet.com>
2023-02-17 18:10:13 +0000

    Initial commit

---
+++ js/alert.js
@@ -0,0 +1,56 @@
+//let name = "Ilya";
+
+
+//alert('hello ${1}'); //hello 1
+
+//alert('hello ${"name"}'); //hello name
+
+//alert('hello ${name}'); //hello Ilya
+
+//Prompt
+//result = prompt(title, [default]);
+
+//Prompt Exercise 
+//let name = prompt("What is your name?", " ");
+
+//alert("Hi " + (name));
+
+
+//Type Conversations 
+
+
+//Numeric Conversation
+//let age = Number("an artbitary string");
+//alert (age); //displays NaN
+
+
+//Comma Operator 
+//let a = (1+2, 3+4);
+//alert (a); //Displays 7
+
+//let a = 1, b=1;
+
+//let c = ++a; //displays 0
+//let d = b++; //displays 2
+//alert (b);
+
+//Results should be 3
+//let a = prompt("First number?", 1);
+//let b = prompt("Second number?", 2);
+
+//alert(+a + +b); // 12
+
+//Comparisons
+//alert(2>1);
+//alert(1>2);
+//alert (2==2);
+
+//alert (5 > 4); //true
+//alert("apple" > "pineapple"); //false
+//alert("2" > "12"); //true -- 2 is greater than 1 from 12
+//alert(undefined == null); //true
+//alert(undefined === null); //false
+//alert(null == "\n0\n"); //false
+//alert(null === +"\n0\n"); //false
+
+
